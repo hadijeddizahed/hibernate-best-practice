229b15dd2f6678949680ecc88b39e846498433f9
Hadi <hadijeddizahed@gmail.com>
2018-06-19 00:15:36 +0430

    add some method to test

--- src/test/java/hibernatetutorial/org/sample/newFeatureTest/NewFeatureTest.java
+++ src/test/java/hibernatetutorial/org/sample/newFeatureTest/NewFeatureTest.java
@@ -2,7 +2,9 @@ package hibernatetutorial.org.sample.newFeatureTest;
 
 
 import hibernatetutorial.org.sample.SampleApplication;
+import hibernatetutorial.org.sample.newFeatures.Person;
 import hibernatetutorial.org.sample.newFeatures.PersonDao;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,6 +12,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.annotation.Resource;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = SampleApplication.class)
@@ -25,5 +32,17 @@ public class NewFeatureTest {
     public static class ContextConfiguration {
     }
 
+
+    @Test
+    public void save_test() {
+        Person person = new Person("Hadi", 32, LocalDate.of(1986, 10, 21));
+        personDao.save(person);
+    }
+
+    @Test
+    public void findMultipleById_test() {
+        List<Person> personList = personDao.getMultipleById(Arrays.asList(1l, 2l, 3l));
+        assertEquals(personList.size(), 3);
+    }
 
 }
